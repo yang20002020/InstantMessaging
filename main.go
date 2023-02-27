42a7c7c16ec30b88d83ffc9a54659bcabe811003
26286 <2628636987@qq.com>
2023-02-27 11:47:19 +0800

    配置数据初始化

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "InstantMessaging/router"
+import (
+	"InstantMessaging/router"
+	"InstantMessaging/utils"
+)
 
 func main() {
+	utils.InitConfig()
+	utils.InitMySql()
 	r := router.Router()
 	r.Run(":8081") //listen and server on 0.0.0.0:8080(for windows "localhost:8080")
 }
---
+++ utils/system_init.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+func InitConfig() {
+	viper.SetConfigName("app")
+	viper.AddConfigPath("config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("err:", err)
+	}
+	fmt.Println("config app:", viper.Get("app"))
+	fmt.Println("config mysql:", viper.Get("mysql"))
+}
+func InitMySql() {
+
+}
