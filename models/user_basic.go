38705860e2076a6c1c3bede62b607d5e2b9f29a0
26286 <2628636987@qq.com>
2023-03-01 22:15:16 +0800

    添加电话和邮箱，对用户信息就行修改并进行验证

--- models/user_basic.go
+++ models/user_basic.go
@@ -11,7 +11,7 @@ type UserBasic struct {
 	gorm.Model
 	Name          string
 	PassWord      string
-	Phone         string `valid:"matches(^1[3-9]\\d{9}$)"`
+	Phone         string `valid:"matches(^1[3-9]{1}\\d{9}$)"`
 	Email         string `valid:"email"`
 	Identity      string
 	ClientIp      string
